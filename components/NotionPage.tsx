a57ef293976450980977c68ed17072936b22b606
dmoore04 <dmoore04@proton.me>
2022-08-20 13:07:25 +0100

    remove favicon sizes

--- components/NotionPage.tsx
+++ components/NotionPage.tsx
@@ -26,7 +26,7 @@ import * as config from 'lib/config'
 import { Loading } from './Loading'
 import { Page404 } from './Page404'
 import { PageHead } from './PageHead'
-// import { PageAside } from './PageAside'
+import { PageAside } from './PageAside'
 import { Footer } from './Footer'
 import { NotionPageHeader } from './NotionPageHeader'
 // import { GitHubShareButton } from './GitHubShareButton'
@@ -198,12 +198,12 @@ export const NotionPage: React.FC<types.PageProps> = ({
   const showTableOfContents = !!isBlogPost
   const minTableOfContentsItems = 3
 
-  //   const pageAside = React.useMemo(
-  //     () => (
-  //       <PageAside block={block} recordMap={recordMap} isBlogPost={isBlogPost} />
-  //     ),
-  //     [block, recordMap, isBlogPost]
-  //   )
+  const pageAside = React.useMemo(
+    () => (
+      <PageAside block={block} recordMap={recordMap} isBlogPost={isBlogPost} />
+    ),
+    [block, recordMap, isBlogPost]
+  )
 
   const footer = React.useMemo(() => <Footer />, [])
 
@@ -282,7 +282,7 @@ export const NotionPage: React.FC<types.PageProps> = ({
         mapPageUrl={siteMapPageUrl}
         mapImageUrl={mapImageUrl}
         searchNotion={config.isSearchEnabled ? searchNotion : null}
-        // pageAside={pageAside}
+        pageAside={pageAside}
         footer={footer}
       />
 
